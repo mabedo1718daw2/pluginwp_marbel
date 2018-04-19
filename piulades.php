badac42e78a40d9595956d2e4486ca1c223be23a
mabedo1718daw2 <mabedo1718daw2@gmail.com>
2018-04-19 23:36:12 +0200

    first commit

---
+++ piulades.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Extensió de WP per twitter
+Plugin URI: https://github.com/mabedo1718daw2/pluginwp_marbel.git
+Description: Una extensió que permet accedir a twitter des de WordPress
+Version: 0.1
+Author: Marc Bellido
+Author URI: https://github.com/mabedo1718daw2
+License: GNU GPLv3
+*/
+?>
